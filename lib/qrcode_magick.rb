6db4bef6ee230eac9f732299a7c38d4e4e897f0d
Terry Finn <terry@terryrfinn.com>
2012-03-15 20:55:38 -0400

    added options parsing

--- lib/qrcode_magick.rb
+++ lib/qrcode_magick.rb
@@ -3,4 +3,27 @@ require 'rqrcode'
 require 'RMagick'
 
 module QRCodeMagick
+  OPTS = {
+    :size => 4,
+    :level => :h,
+    :scale => 1,
+    :write_to => nil,
+    :drawing => Magick::Draw.new,
+    :canvas => nil
+  }
+  
+  def self.draw(string, *args)
+    self.parse_options(args)
+  end
+  
+  private
+  
+  def self.parse_options(args)
+    if hash = args.first
+      Hash[hash].each do |k,v|
+        raise ArgumentError, "unknow option: #{k}" unless OPTS.key?(k)
+        OPTS[k] = v
+      end
+    end
+  end
 end
