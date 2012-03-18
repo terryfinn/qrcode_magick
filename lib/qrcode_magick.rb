d1b0f6ccdc4682ac791f205f1a639f21fc1c7ec1
Terry Finn <terry@terryrfinn.com>
2012-03-18 10:36:40 -0400

    default options are now defined in a method that returns a hash literal

--- lib/qrcode_magick.rb
+++ lib/qrcode_magick.rb
@@ -3,40 +3,48 @@ require 'rqrcode'
 require 'RMagick'
 
 module QRCodeMagick
-  OPTS = {
-    :size => 4,
-    :level => :h,
-    :scale => 1,
-    :write_to => nil,
-    :drawing => Magick::Draw.new,
-    :canvas => nil
-    :fill => Magick::HatchFill.new('white')
-  }
   
   def self.draw(string, *args)
-    self.parse_options(args)
-    qrcode = RQRCode::QRCode.new(string, :size => OPTS[:size], :level => OPTS[:level])
-    OPTS[:drawing] = self.generate_drawing(qrcode, OPTS[:drawing], OPTS[:scale])
-    OPTS[:canvas] = self.generate_canvas(qrcode, OPTS[:scale], OPTS[:fill]) unless OPTS[:canvas]
+    opts = self.defaults
+    self.parse_options(args, opts)
+    qrcode = RQRCode::QRCode.new(string, :size => opts[:size], :level => opts[:level])
+    opts[:drawing] = self.generate_drawing(qrcode, opts[:drawing], opts[:scale])
+    opts[:canvas] = self.generate_canvas(qrcode, opts[:scale], opts[:fill]) unless opts[:canvas]
+    opts[:drawing].draw(opts[:canvas])
+    opts[:canvas].write(opts[:write_to]) if opts[:write_to]
+    opts[:canvas]
   end
   
   private
   
-  def self.parse_options(args)
+  def self.defaults
+    {
+      :size => 4,
+      :level => :h,
+      :scale => 2,
+      :write_to => nil,
+      :drawing => Magick::Draw.new,
+      :canvas => nil,
+      :fill => Magick::HatchFill.new('white')
+    }
+  end
+  
+  def self.parse_options(args, opts)
     if hash = args.first
       Hash[hash].each do |k,v|
-        raise ArgumentError, "unknow option: #{k}" unless OPTS.key?(k)
-        OPTS[k] = v
+        raise ArgumentError, "unknown option: #{k}" unless opts.key?(k)
+        opts[k] = v
       end
     end
   end
   
   def self.generate_canvas(qrcode, scale, fill)
     canvas_side = (qrcode.module_count * scale) + (2 * scale)
+    puts "(#{qrcode.module_count} * #{scale}) + (2 * #{scale}) = #{canvas_side}"
     Magick::Image.new(canvas_side, canvas_side, fill)
   end
   
-  def self.generate_drawing(qrcode, drawing, scale)
+  def self.generate_drawing(qrcode, drawing, box_size)
     offset = scale - 1
     
     qrcode.module_count.times do |row|
